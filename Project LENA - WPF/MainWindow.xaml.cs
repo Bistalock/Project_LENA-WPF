720bec5328e8fad25ad1fea40a33e9bc45119955
Bistalock <gdelpino92@gmail.com>
2014-09-27 17:04:42 -0500

    Cleaned up some P/Invoke code

--- Project LENA - WPF/MainWindow.xaml.cs
+++ Project LENA - WPF/MainWindow.xaml.cs
@@ -72,158 +72,9 @@ namespace Project_LENA___WPF
         /// Window message values, WM_*
         /// </summary>
         internal enum WM
-        {
-            NULL = 0x0000,
-            CREATE = 0x0001,
-            DESTROY = 0x0002,
-            MOVE = 0x0003,
-            SIZE = 0x0005,
-            ACTIVATE = 0x0006,
-            SETFOCUS = 0x0007,
-            KILLFOCUS = 0x0008,
-            ENABLE = 0x000A,
-            SETREDRAW = 0x000B,
-            SETTEXT = 0x000C,
-            GETTEXT = 0x000D,
-            GETTEXTLENGTH = 0x000E,
-            PAINT = 0x000F,
-            CLOSE = 0x0010,
-            QUERYENDSESSION = 0x0011,
-            QUIT = 0x0012,
-            QUERYOPEN = 0x0013,
-            ERASEBKGND = 0x0014,
-            SYSCOLORCHANGE = 0x0015,
-            SHOWWINDOW = 0x0018,
-            CTLCOLOR = 0x0019,
-            WININICHANGE = 0x001A,
-            SETTINGCHANGE = 0x001A,
-            ACTIVATEAPP = 0x001C,
-            SETCURSOR = 0x0020,
-            MOUSEACTIVATE = 0x0021,
-            CHILDACTIVATE = 0x0022,
-            QUEUESYNC = 0x0023,
-            GETMINMAXINFO = 0x0024,
-
-            WINDOWPOSCHANGING = 0x0046,
-            WINDOWPOSCHANGED = 0x0047,
-
-            CONTEXTMENU = 0x007B,
-            STYLECHANGING = 0x007C,
-            STYLECHANGED = 0x007D,
-            DISPLAYCHANGE = 0x007E,
-            GETICON = 0x007F,
-            SETICON = 0x0080,
-            NCCREATE = 0x0081,
-            NCDESTROY = 0x0082,
-            NCCALCSIZE = 0x0083,
-            NCHITTEST = 0x0084,
-            NCPAINT = 0x0085,
-            NCACTIVATE = 0x0086,
-            GETDLGCODE = 0x0087,
-            SYNCPAINT = 0x0088,
-            NCMOUSEMOVE = 0x00A0,
-            NCLBUTTONDOWN = 0x00A1,
-            NCLBUTTONUP = 0x00A2,
-            NCLBUTTONDBLCLK = 0x00A3,
-            NCRBUTTONDOWN = 0x00A4,
-            NCRBUTTONUP = 0x00A5,
-            NCRBUTTONDBLCLK = 0x00A6,
-            NCMBUTTONDOWN = 0x00A7,
-            NCMBUTTONUP = 0x00A8,
-            NCMBUTTONDBLCLK = 0x00A9,
-
-            SYSKEYDOWN = 0x0104,
-            SYSKEYUP = 0x0105,
-            SYSCHAR = 0x0106,
-            SYSDEADCHAR = 0x0107,
-            COMMAND = 0x0111,
-            SYSCOMMAND = 0x0112,
-
-            MOUSEMOVE = 0x0200,
-            LBUTTONDOWN = 0x0201,
-            LBUTTONUP = 0x0202,
-            LBUTTONDBLCLK = 0x0203,
-            RBUTTONDOWN = 0x0204,
-            RBUTTONUP = 0x0205,
-            RBUTTONDBLCLK = 0x0206,
-            MBUTTONDOWN = 0x0207,
-            MBUTTONUP = 0x0208,
-            MBUTTONDBLCLK = 0x0209,
-            MOUSEWHEEL = 0x020A,
-            XBUTTONDOWN = 0x020B,
-            XBUTTONUP = 0x020C,
-            XBUTTONDBLCLK = 0x020D,
-            MOUSEHWHEEL = 0x020E,
-            PARENTNOTIFY = 0x0210,
-
-            CAPTURECHANGED = 0x0215,
-            POWERBROADCAST = 0x0218,
-            DEVICECHANGE = 0x0219,
-
-            ENTERSIZEMOVE = 0x0231,
-            EXITSIZEMOVE = 0x0232,
-
-            IME_SETCONTEXT = 0x0281,
-            IME_NOTIFY = 0x0282,
-            IME_CONTROL = 0x0283,
-            IME_COMPOSITIONFULL = 0x0284,
-            IME_SELECT = 0x0285,
-            IME_CHAR = 0x0286,
-            IME_REQUEST = 0x0288,
-            IME_KEYDOWN = 0x0290,
-            IME_KEYUP = 0x0291,
-
-            NCMOUSELEAVE = 0x02A2,
-
-            TABLET_DEFBASE = 0x02C0,
-            //WM_TABLET_MAXOFFSET = 0x20,
-
-            TABLET_ADDED = TABLET_DEFBASE + 8,
-            TABLET_DELETED = TABLET_DEFBASE + 9,
-            TABLET_FLICK = TABLET_DEFBASE + 11,
-            TABLET_QUERYSYSTEMGESTURESTATUS = TABLET_DEFBASE + 12,
-
-            CUT = 0x0300,
-            COPY = 0x0301,
-            PASTE = 0x0302,
-            CLEAR = 0x0303,
-            UNDO = 0x0304,
-            RENDERFORMAT = 0x0305,
-            RENDERALLFORMATS = 0x0306,
-            DESTROYCLIPBOARD = 0x0307,
-            DRAWCLIPBOARD = 0x0308,
-            PAINTCLIPBOARD = 0x0309,
-            VSCROLLCLIPBOARD = 0x030A,
-            SIZECLIPBOARD = 0x030B,
-            ASKCBFORMATNAME = 0x030C,
-            CHANGECBCHAIN = 0x030D,
-            HSCROLLCLIPBOARD = 0x030E,
-            QUERYNEWPALETTE = 0x030F,
-            PALETTEISCHANGING = 0x0310,
-            PALETTECHANGED = 0x0311,
-            HOTKEY = 0x0312,
-            PRINT = 0x0317,
-            PRINTCLIENT = 0x0318,
-            APPCOMMAND = 0x0319,
-            THEMECHANGED = 0x031A,
-
-            DWMCOMPOSITIONCHANGED = 0x031E,
-            DWMNCRENDERINGCHANGED = 0x031F,
-            DWMCOLORIZATIONCOLORCHANGED = 0x0320,
-            DWMWINDOWMAXIMIZEDCHANGE = 0x0321,
-
-            GETTITLEBARINFOEX = 0x033F,
-            #region Windows 7
-            DWMSENDICONICTHUMBNAIL = 0x0323,
-            DWMSENDICONICLIVEPREVIEWBITMAP = 0x0326,
-            #endregion
-
-            USER = 0x0400,
-
-            // This is the hard-coded message value used by [....] for Shell_NotifyIcon.
-            // It's relatively safe to reuse.
-            TRAYMOUSEMESSAGE = 0x800, //WM_USER + 1024
-            APP = 0x8000,
+        {          
+            NCHITTEST = 0x0084, //         
+            SYSCOMMAND = 0x0112, //
         }
 
         /// <summary>
@@ -232,31 +83,6 @@ namespace Project_LENA___WPF
         internal enum SC
         {
             SIZE = 0xF000,
-            MOVE = 0xF010,
-            MINIMIZE = 0xF020,
-            MAXIMIZE = 0xF030,
-            NEXTWINDOW = 0xF040,
-            PREVWINDOW = 0xF050,
-            CLOSE = 0xF060,
-            VSCROLL = 0xF070,
-            HSCROLL = 0xF080,
-            MOUSEMENU = 0xF090,
-            KEYMENU = 0xF100,
-            ARRANGE = 0xF110,
-            RESTORE = 0xF120,
-            TASKLIST = 0xF130,
-            SCREENSAVE = 0xF140,
-            HOTKEY = 0xF150,
-            DEFAULT = 0xF160,
-            MONITORPOWER = 0xF170,
-            CONTEXTHELP = 0xF180,
-            SEPARATOR = 0xF00F,
-            /// <summary>
-            /// SCF_ISSECURE
-            /// </summary>
-            F_ISSECURE = 0x00000001,
-            ICON = MINIMIZE,
-            ZOOM = MAXIMIZE,
         }
 
         /// <summary>
@@ -265,53 +91,11 @@ namespace Project_LENA___WPF
         [Flags]
         internal enum WS : uint
         {
-            OVERLAPPED = 0x00000000,
-            POPUP = 0x80000000,
-            CHILD = 0x40000000,
-            MINIMIZE = 0x20000000,
-            VISIBLE = 0x10000000,
-            DISABLED = 0x08000000,
-            CLIPSIBLINGS = 0x04000000,
-            CLIPCHILDREN = 0x02000000,
-            MAXIMIZE = 0x01000000,
-            BORDER = 0x00800000,
-            DLGFRAME = 0x00400000,
-            VSCROLL = 0x00200000,
-            HSCROLL = 0x00100000,
-            SYSMENU = 0x00080000,
-            THICKFRAME = 0x00040000,
-            GROUP = 0x00020000,
-            TABSTOP = 0x00010000,
-
-            MINIMIZEBOX = 0x00020000,
             MAXIMIZEBOX = 0x00010000,
-
-            CAPTION = BORDER | DLGFRAME,
-            TILED = OVERLAPPED,
-            ICONIC = MINIMIZE,
-            SIZEBOX = THICKFRAME,
-            TILEDWINDOW = OVERLAPPEDWINDOW,
-
-            OVERLAPPEDWINDOW = OVERLAPPED | CAPTION | SYSMENU | THICKFRAME | MINIMIZEBOX | MAXIMIZEBOX,
-            POPUPWINDOW = POPUP | BORDER | SYSMENU,
-            CHILDWINDOW = CHILD,
         }
 
         internal enum HT
-        {
-            ERROR = -2,
-            TRANSPARENT = -1,
-            NOWHERE = 0,
-            CLIENT = 1,
-            CAPTION = 2,
-            SYSMENU = 3,
-            GROWBOX = 4,
-            SIZE = GROWBOX,
-            MENU = 5,
-            HSCROLL = 6,
-            VSCROLL = 7,
-            MINBUTTON = 8,
-            MAXBUTTON = 9,
+        {          
             LEFT = 10,
             RIGHT = 11,
             TOP = 12,
@@ -321,29 +105,6 @@ namespace Project_LENA___WPF
             BOTTOMLEFT = 16,
             BOTTOMRIGHT = 17,
             BORDER = 18,
-            REDUCE = MINBUTTON,
-            ZOOM = MAXBUTTON,
-            SIZEFIRST = LEFT,
-            SIZELAST = BOTTOMRIGHT,
-            OBJECT = 19,
-            CLOSE = 20,
-            HELP = 21
-        }
-
-        /// <summary>
-        /// EnableMenuItem uEnable values, MF_*
-        /// </summary>
-        [Flags]
-        internal enum MF : uint
-        {
-            /// <summary>
-            /// Possible return value for EnableMenuItem
-            /// </summary>
-            DOES_NOT_EXIST = unchecked((uint)-1),
-            ENABLED = 0,
-            BYCOMMAND = 0,
-            GRAYED = 1,
-            DISABLED = 2,
         }
 
         [DllImport("user32.dll")]
@@ -377,35 +138,6 @@ namespace Project_LENA___WPF
         // Variables having to do with the resizing code follow
         protected bool bSizeMode = false;
 
-        //// <summary>
-        ///// Raise BeginResize event
-        ///// </summary>
-        ///// <param name="e"></param>
-        //protected virtual void OnBeginResize(System.EventArgs e)
-        //{
-        //    if (BeginResize != null)
-        //        BeginResize(this, e);
-        //}
-
-        ///// <summary>
-        ///// Raise EndResize event
-        ///// </summary>
-        ///// <param name="e"></param>
-        //protected virtual void OnEndResize(System.EventArgs e)
-        //{
-        //    if (EndResize != null)
-        //        EndResize(this, e);
-        //}
-
-        ///// <summary>
-        ///// Fires once at the beginning of a resizing drag.
-        ///// </summary>
-        //public event EventHandler BeginResize;
-
-        ///// <summary>
-        ///// Fires once at the end of a resizing drag.
-        ///// </summary>
-        //public event EventHandler EndResize;
 
         private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
         {
@@ -568,7 +300,7 @@ namespace Project_LENA___WPF
                     else if (Processing_Image.IsSelected)
                     {
                         if (IsProcessing == true) this.AnimateWindowSize(650);
-                        else if (radioButton3.IsChecked == true || radioButton4.IsChecked == true) this.AnimateWindowSize(360);
+                        else if (radioButton3.IsChecked == true || radioButton4.IsChecked == true) this.AnimateWindowSize(365);
                         else this.AnimateWindowSize(220);
                     }
                     else if (About.IsSelected) this.AnimateWindowSize(400);
@@ -806,14 +538,6 @@ namespace Project_LENA___WPF
             if (pixel == 3)
             {
                 double noise = Convert.ToDouble(textBox2.Text);
-
-                //// Obtain basic tag information of the image
-                //#region GetTagInfo
-                //int width = image.GetField(TiffTag.IMAGEWIDTH)[0].ToInt();
-                //int height = image.GetField(TiffTag.IMAGELENGTH)[0].ToInt();
-                //byte bits = image.GetField(TiffTag.BITSPERSAMPLE)[0].ToByte();
-                //byte pixel = image.GetField(TiffTag.SAMPLESPERPIXEL)[0].ToByte();
-                //#endregion
 
                 int test = (int)bits;
 
